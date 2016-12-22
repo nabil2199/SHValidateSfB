d633615008728d2a33329aed2ea4be4edf148549
nabil2199 <nabil2199@gmail.com>
2016-12-22 15:57:12 +0100

    initial account

---
+++ SHValidateSfB.ps1
@@ -0,0 +1,48 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function Validate()
+{
+    Param(
+        [string]$Test,
+        [bool]  $Condition,
+        [string]$FailureMsg,
+        [switch]$WarningOnly
+    )
+
+    Write-Host -NoNewline -ForegroundColor White $Test.PadRight(100,'.')
+    if ($Condition)
+    {
+        Write-Host -ForegroundColor Green "Passed"
+        $global:iTotalPasses++
+    }
+    else
+    {
+        if ($WarningOnly)
+        {
+            Write-Host -ForegroundColor Yellow ("Warning: "+$FailureMsg)
+            $global:iTotalWarnings++
+        }
+        else
+        {
+            Write-Host -ForegroundColor Red ("Failed: "+$FailureMsg)
+            $global:iTotalFailures++
+        }
+    }
+}
