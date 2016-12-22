6531e3d5abe62b91b771b6ebf9889165a0f924db
nabil2199 <nabil2199@gmail.com>
2016-12-22 16:06:56 +0100

    function validate

--- SHValidateSfB.ps1
+++ SHValidateSfB.ps1
@@ -1,22 +1,3 @@
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 function Validate()
 {
     Param(
@@ -46,3 +27,29 @@ function Validate()
         }
     }
 }
+
+## SfB ##
+$strLyncIdentity = $null
+if ($fSfbIsOnline)
+{
+    $strLyncIdentity = $strUpn
+}
+else
+{
+    $strLyncIdentity = $strAlias
+}
+
+$lyncAccount = $null
+try {
+    $lyncAccount = Get-CsMeetingRoom -Identity $strLyncIdentity -WarningAction SilentlyContinue -ErrorAction SilentlyContinue
+} catch {
+    try {
+        $lyncAccount = Get-CsUser -Identity $strLyncIdentity -WarningAction SilentlyContinue -ErrorAction SilentlyContinue
+    } catch { }
+}
+Validate -Test "There is a Lync or Skype for Business account for $strLyncIdentity" -Condition ($lyncAccount -ne $null -and $lyncAccount.Enabled) -FailureMsg "SfB Enabled - there is no Skype for Business account - meetings will not support Skype for Business"
+if ($lyncAccount)
+{
+    Validate -Test "The meeting room has a SIP address" -Condition (![System.String]::IsNullOrEmpty($lyncAccount.SipAddress)) -FailureMsg "SfB Enabled - there is no SIP Address - the device account cannot be used to sign into Skype for Business."
+}
+## End SFB ##
